b40b5a07d8332eb1c3522e9823ce9afa19677e20
Andromeda <eclipse0010101@gmail.com>
2023-10-13 18:35:25 -0500

    Add files via upload

---
+++ includes/PowerPwn.h
@@ -0,0 +1,20 @@
+#include <cstdint>
+
+#ifndef PowerPwn
+#define PowerPwn
+
+const uint8_t CMDLINE_BUFFER = 32;
+const uint16_t BUFFER = 1024;
+
+struct commands {
+    std::string exit;
+};
+
+uint8_t sd(void);
+void green(void);
+void red(void);
+int options(void);
+void main_loop(void);
+void create_connection(char ip[CMDLINE_BUFFER]);
+
+#endif
